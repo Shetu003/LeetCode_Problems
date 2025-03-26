a2935c5db1a47517018a8d59de3af0c7fdbe72fd
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-03-26 22:24:06 +0530

    Time: 2 ms (75.14%), Space: 53.3 MB (38.22%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,39 @@
+//Moore's Voting Algorithm
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int cand=nums[0];
+        int votes=0;
+        for(int i=0;i<nums.length;i++){
+            if(votes==0){
+                cand=nums[i];
+                votes++;
+            }
+            else if(cand==nums[i]){
+                votes++;
+            }
+            else{
+                votes--;
+            }
+        }
+        return cand;
+    }
+}
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int cand=0;
+//         int count=0;
+//         for(int i=0; i<nums.length; i++){
+//             if(count==0){
+//                 cand = nums[i];
+//             }
+//             if(cand == nums[i]){
+//                 count++;
+//             }
+//             else{
+//                 count--;
+//             }
+//         }
+//         return cand;
+//     }
+// }
