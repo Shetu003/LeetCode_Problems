975624471b08eb48ca6094a9044bc1f23139688c
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-03-27 19:23:51 +0530

    Time: 11 ms (95.51%), Space: 63.8 MB (10.9%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -1,33 +1,39 @@
 class Solution {
-    public int minimumIndex(List<Integer> list) {
-        int n = list.size();
-        int max = Integer.MIN_VALUE;
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (count == 0)
-                max = list.get(i);
-            if (max == list.get(i))
-                count++;
-            else
-                count--;
-        }
-        count = 0;
-        for (int num : list) {
-            if (num == max)
-                count++;
-        }
-        if (count <= (n - count) + 1)
-            return -1;
+    public int minimumIndex(List<Integer> nums) {
+    // First find the majority element (using Moore's Voting Algorithm)
+    // then, using the prefix & suffix count find the split idx
 
-        count = 0;
-        for (int i = 0; i < n; i++) {
-            if (list.get(i) == max)
-                count++;
+        int n = nums.size();
+        int maj = -1;
+        int cnt = 0;
+        for (int num : nums) {
+            if (cnt == 0) {
+                maj = num;
+                cnt = 1;
+            }
+            else if (num == maj)
+                cnt++;
             else
-                count--;
-            if (count == 1)
+                cnt--;
+        }
+
+        int leftCnt = 0;
+        int rightCnt = 0;
+        for (int num : nums) {
+            if (num == maj)
+                rightCnt++;
+        }
+
+        for (int i = 0; i < n-1; i++) {
+            if (nums.get(i) == maj) {
+                leftCnt++;
+                rightCnt--;
+            }
+            
+            if (leftCnt > (i+1)/2 && rightCnt > (n-1-i)/2)
                 return i;
         }
-        return -1;
+
+        return -1;   
     }
 }
