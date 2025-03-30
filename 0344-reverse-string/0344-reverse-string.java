c4730a8702ac5525fea29f394dcc98da54a458d2
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-03-30 23:06:40 +0530

    Time: 0 ms (100%), Space: 49.4 MB (36.12%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,8 +1,21 @@
-class Solution {
-    public void reverseString(char[] s) {
-        StringBuilder sb = new StringBuilder(new String(s)).reverse();
-        for(int i=0; i<s.length; i++){
-            s[i] = sb.charAt(i);
+class Solution{
+    public void reverseString(char[]s){
+        int left=0,right=s.length-1;
+        while(left<right){
+            char temp=s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
         }
     }
 }
+
+// class Solution {
+//     public void reverseString(char[] s) {
+//         StringBuilder sb = new StringBuilder(new String(s)).reverse();
+//         for(int i=0; i<s.length; i++){
+//             s[i] = sb.charAt(i);
+//         }
+//     }
+// }
