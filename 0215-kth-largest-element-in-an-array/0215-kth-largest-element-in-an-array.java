ccd5b98725d912a92ae921c20bc6165830ad2ee8
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-04-02 09:59:09 +0530

    Time: 70 ms (19.59%), Space: 58.1 MB (54.11%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,15 +1,41 @@
+//using max priority queue 
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for(int i=0; i<nums.length; i++){
             pq.add(nums[i]);
-            if(pq.size() > k){
-                pq.poll();
-            }
+        }
+        int f = k-1;
+        while(f>0){
+            pq.poll();
+            f--;
         }
         return pq.peek();        
     }
 }
+//using min priority queue
+// class Solution {
+//     public int findKthLargest(int[] nums, int k) {
+//         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+//         for (int i=0; i<nums.length; i++) {
+//             minHeap.add(nums[i]);
+//             if (minHeap.size() > k) {
+//                 minHeap.poll();
+//             }
+//         }
+//         return minHeap.peek();        
+//     }
+// }
+
+// USING SORTING 
+
+// class Solution {
+//     public int findKthLargest(int[] nums, int k) {
+//         Arrays.sort(nums);
+//         return nums[nums.length - k];        
+//     }
+// }
 //using sorting
 // class Solution {
 //     public int findKthLargest(int[] nums, int k) {
