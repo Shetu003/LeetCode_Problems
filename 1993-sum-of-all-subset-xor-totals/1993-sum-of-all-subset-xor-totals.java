3ce92ca6cfd493c371bb1a44d3fe1fe7b296daf4
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-04-06 23:22:47 +0530

    Time: 0 ms (100%), Space: 41.4 MB (24.86%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
@@ -1,6 +1,6 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        int n = nums.length; // Number of elements in nums
+        int n = nums.length;
         int sum = 0;
         for (int num : nums) { // Bitwise OR all elements
             sum |= num;
