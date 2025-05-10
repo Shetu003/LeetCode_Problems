30f947f15006458326c183f6b690fdc01ea0a7ab
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-05-10 14:27:33 +0530

    Time: 3 ms (47.7%), Space: 42.2 MB (23.64%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -3,17 +3,17 @@ class Solution {
         Stack<Integer> st = new Stack<>();
         for(String ops : operations){
             if(ops.equals("+")){
-                int a = st.pop();
-                int b = st.peek();
-                st.push(a);
-                st.push(a + b);
+                int a = st.pop();    // Last score
+                int b = st.peek();   // Second last score
+                st.push(a);          // Push back the last score
+                st.push(a + b);      // Push their sum
             }
-            else if(ops.equals("D")) st.push(st.peek() * 2);
-            else if(ops.equals("C")) st.pop();
+            else if(ops.equals("D")) st.push(st.peek() * 2);    // Double the last score
+            else if(ops.equals("C")) st.pop();                  //remove last score 
             else st.push(Integer.valueOf(ops));
         }
         int sum=0;
-        while(!st.isEmpty()){
+        while(!st.isEmpty()){       // Add up all the scores to get result
             sum += st.pop();
         }
         return sum;
