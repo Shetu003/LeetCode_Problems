1d90e8b65c41063dda515cd7a598a359c61edaf8
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-05-13 18:35:53 +0530

    Time: 154 ms (10.1%), Space: 45.7 MB (8.03%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -2,12 +2,12 @@ class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
         Set<List<Integer>> set = new HashSet<>();
-        List<List<Integer>> res =  new ArrayList<>();
-
+        List<List<Integer>> list = new ArrayList<>();
         for(int i=0; i<nums.length; i++){
             for(int j=i+1; j<nums.length; j++){
-                int k=j+1;
-                int l=nums.length-1;
+                int k = j+1;
+                int l = nums.length-1;
+
                 while(k<l){
                     long sum = nums[i];
                     sum += nums[j];
@@ -28,7 +28,8 @@ class Solution {
                 }
             }
         }
-        res.addAll(set);
-        return res;
+        list.addAll(set);
+        return list;
     }
 }
+
