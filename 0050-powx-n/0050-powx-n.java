7e3dba9fdf6ef5efcb4093a67cdc4bdfcfc82d22
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-05-18 12:22:08 +0530

    Time: 0 ms (100%), Space: 43.5 MB (10.61%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,15 +1,15 @@
 class Solution {
     public double myPow(double x, int n) {
-        if (n == 0) 
+        if (n == 0)
             return 1;
 
-        if (n % 2 == 1) 
+        if (n % 2 == 1) //If n is odd, reduce the power by 1 to make it even, and multiply x once.
             return x * myPow(x, n - 1);
 
-        if (n % 2 == 0) 
+        if (n % 2 == 0) //If n is even, use exponentiation by squaring
             return myPow(x * x, n / 2);
 
-        return 1 / myPow(x, -n);
+        return 1 / myPow(x, -n); //This handles negative exponents
     }
 }
 // TC: O(logn), SC: O(1)
