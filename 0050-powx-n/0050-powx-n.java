7c3c37e4bc7a5572c88939e6b1dbdd2dc5ae2087
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-05-18 12:22:58 +0530

    Time: 0 ms (100%), Space: 43.3 MB (20.47%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -13,3 +13,28 @@ class Solution {
     }
 }
 // TC: O(logn), SC: O(1)
+
+
+// class Solution {
+//     public double myPow(double x, int n) {
+//         double ans = 1;
+//         long power = n; 
+
+//         if (power < 0) {
+//             x = 1 / x; 
+//             power = -power; 
+//         }
+
+//         while(power>0){
+//             if(power%2!=0){
+//                 ans *= x;
+//                 power -= 1;
+//             }
+//             else{
+//                 x *= x;
+//                 power /= 2;
+//             }
+//         }
+//         return ans;
+//     }
+// }
