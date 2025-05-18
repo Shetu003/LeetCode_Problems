820a774d3d0e7fd96b1ab7418fe923a3f277c715
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-05-18 12:20:32 +0530

    Time: 0 ms (100%), Space: 43.1 MB (24.04%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,7 +1,7 @@
 class Solution {
     public double myPow(double x, int n) {
         if (n == 0) 
-            return 1.0;
+            return 1;
 
         if (n % 2 == 1) 
             return x * myPow(x, n - 1);
