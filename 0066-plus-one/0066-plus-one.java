0093f5277b8d667315962380a0ea967e7c98f702
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-05-23 00:31:19 +0530

    Time: 0 ms (100%), Space: 42 MB (31.51%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,15 +1,20 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-        for(int i=n-1; i>=0; i--){
-            if(digits[i] < 9){
+
+        for(int i = n - 1; i >= 0; i--) {
+            // If digit less than 9, increment and return result immediately
+            if(digits[i] < 9) {
                 digits[i]++;
                 return digits;
             }
+            // If digit is 9, set it to 0 
             digits[i] = 0;
         }
-        int[] newNo = new int[n+1];
-        newNo[0] = 1;
+
+        // If all digits were 9, result will have one more digit (e.g., 999 + 1 = 1000)
+        int[] newNo = new int[n + 1];
+        newNo[0] = 1; // Set first digit to 1 and rest default 0
         return newNo;
     }
 }
