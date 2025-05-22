37f5fb2acf1fefdcc3139348d5917afa9108fd3a
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-05-23 00:06:29 +0530

    Time: 2 ms (97.41%), Space: 56.1 MB (68.55%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,17 +1,31 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int currTank = 0;
-        int totalTank = 0;
-        int startStation = 0;
-        for(int i=0; i<gas.length; i++){
-            currTank += gas[i] - cost[i];
-            totalTank += gas[i] - cost[i];
-
-            if(currTank < 0){
-                startStation = i+1;
-                currTank = 0;
+        int startStation = 0, totalGas = 0, curGas = 0;
+        for (int i = 0; i < gas.length; i++) {
+            curGas += gas[i] - cost[i];
+            if (curGas < 0) {
+                startStation = i + 1;
+                curGas = 0;
             }
+            totalGas += gas[i] - cost[i];
         }
-        return totalTank >= 0 ? startStation : -1;
+        return totalGas < 0 ? -1 : startStation;
     }
 }
+// class Solution {
+//     public int canCompleteCircuit(int[] gas, int[] cost) {
+//         int currTank = 0;
+//         int totalTank = 0;
+//         int startStation = 0;
+//         for(int i=0; i<gas.length; i++){
+//             currTank += gas[i] - cost[i];
+//             totalTank += gas[i] - cost[i];
+
+//             if(currTank < 0){
+//                 startStation = i+1;
+//                 currTank = 0;
+//             }
+//         }
+//         return totalTank >= 0 ? startStation : -1;
+//     }
+// }
