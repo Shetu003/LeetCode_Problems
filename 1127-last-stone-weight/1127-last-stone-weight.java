265a42cf7d23fb813dad2ada2cb6ce832a01cf6d
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-05-24 19:18:09 +0530

    Time: 1 ms (97.97%), Space: 41.2 MB (56.55%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.java
+++ 1127-last-stone-weight/1127-last-stone-weight.java
@@ -1,23 +1,18 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a); //to make it a max-heap using custom comparator:
         for(int c : stones){
-            pq.add(c);
+            pq.add(c); //Add all stones into max-heap
         }
         while(pq.size()>1){
-            int top1 = pq.poll();
-            int top2 = pq.poll();
+            int top1 = pq.poll(); //heaviest stone
+            int top2 = pq.poll(); //2nd heaviest stone
             int diff = Math.abs(top1-top2);
 
-            if(diff != 0){
-                pq.add(diff);
+            if(diff != 0){ 
+                pq.add(diff); //x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x(diff)
             }
         }
-        if(pq.size() != 0){
-            return pq.poll();
-        }
-        else{
-            return 0;
-        }
+        return pq.size() != 0 ? pq.poll() : 0;
     }
 }
