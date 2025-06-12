c44cbae46e3151a930e899c5f58b8d211d72b829
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-06-12 18:16:19 +0530

    Time: 1 ms (54.2%), Space: 42.2 MB (50.66%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -10,33 +10,28 @@ class Solution {
         phoneMap.put('7', "pqrs");
         phoneMap.put('8', "tuv");
         phoneMap.put('9', "wxyz");
-        
+
         List<String> res = new ArrayList<>();
-        if(digits == null || digits.length() == 0){
+
+        if (digits == null || digits.length() == 0) { //edge case 
             return res;
         }
-        
-        backtrack(0, new StringBuilder(), digits, phoneMap, res);
+
+        backtrack(0, new StringBuilder(), digits, phoneMap, res); //i = 0 — starting index , path = "" — current combination, digits = "23", res = [] — result list
         return res;
     }
 
-    private void backtrack(int i, StringBuilder path, String digits, Map<Character, String> phoneMqp, List<String> res){
-        // while i < digits .length
-         // get new  first index of Map
-           // make a call to get index plus 1
-            // then remove the last value
-
-        if(i == digits.length()){
+    private void backtrack(int i, StringBuilder path, String digits, Map<Character, String> phoneMap,List<String> res) {
+        if (i == digits.length()) { //i=0 != 2, then skip 
             res.add(path.toString());
             return;
         }
 
-        String letters = phoneMqp.get(digits.charAt(i));
-        for(char c: letters.toCharArray()){
-            path.append(c);
-            backtrack(i +1, path, digits, phoneMqp, res);
-            path.deleteCharAt(path.length() -1);
+        String letters = phoneMap.get(digits.charAt(i)); //Get the digit at current index i, and find the corresponding letters i.e, '2' -> "abc"
+        for (char c : letters.toCharArray()) {
+            path.append(c); //iterate through the letter of digit and add current letter to path i.e, "a"
+            backtrack(i + 1, path, digits, phoneMap, res); //move to the next digit 
+            path.deleteCharAt(path.length() - 1);
         }
-
     }
 }
