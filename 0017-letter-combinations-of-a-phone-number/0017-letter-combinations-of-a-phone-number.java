0379a3b51b3c5cd21bb4043164d207a56a83534b
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-06-12 18:04:50 +0530

    Time: 0 ms (100%), Space: 42.1 MB (77.4%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,23 +1,42 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        List<String> ansList = new ArrayList<>();
-        String combi = "";
 
-        String[] arr = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        if (digits == null || digits.isEmpty()) return ansList;
-        findCombination(digits, ansList, 0, combi, arr);
-        return ansList;
+        Map<Character, String> phoneMap = new HashMap<>();
+        phoneMap.put('2', "abc");
+        phoneMap.put('3', "def");
+        phoneMap.put('4', "ghi");
+        phoneMap.put('5', "jkl");
+        phoneMap.put('6', "mno");
+        phoneMap.put('7', "pqrs");
+        phoneMap.put('8', "tuv");
+        phoneMap.put('9', "wxyz");
+        
+        List<String> res = new ArrayList<>();
+        if(digits == null || digits.length() == 0){
+            return res;
+        }
+        
+        backtrack(0, new StringBuilder(), digits, phoneMap, res);
+        return res;
     }
 
-    private void findCombination(String digits, List<String> ansList, int index, String combi, String[] arr){
-        if(combi.length() == digits.length()){
-            ansList.add(combi);
+    private void backtrack(int i, StringBuilder path, String digits, Map<Character, String> phoneMqp, List<String> res){
+        // while i < digits .length
+         // get new  first index of Map
+           // make a call to get index plus 1
+            // then remove the last value
+
+        if(i == digits.length()){
+            res.add(path.toString());
             return;
         }
-           String str = arr[digits.charAt(index) - '0'];
 
-        for(int i=0; i < str.length(); i++){
-            findCombination(digits, ansList, index + 1, combi + str.charAt(i), arr);
+        String letters = phoneMqp.get(digits.charAt(i));
+        for(char c: letters.toCharArray()){
+            path.append(c);
+            backtrack(i +1, path, digits, phoneMqp, res);
+            path.deleteCharAt(path.length() -1);
         }
+
     }
 }
