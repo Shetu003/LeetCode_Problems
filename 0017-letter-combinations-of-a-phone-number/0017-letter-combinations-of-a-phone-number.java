32c2561a0614cc7cd9535a05b599de3fee369d29
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-06-12 18:19:07 +0530

    Time: 4 ms (34.72%), Space: 42.5 MB (26.36%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,37 +1,23 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
+        List<String> ansList = new ArrayList<>();
+        String combi = "";
 
-        Map<Character, String> phoneMap = new HashMap<>();
-        phoneMap.put('2', "abc");
-        phoneMap.put('3', "def");
-        phoneMap.put('4', "ghi");
-        phoneMap.put('5', "jkl");
-        phoneMap.put('6', "mno");
-        phoneMap.put('7', "pqrs");
-        phoneMap.put('8', "tuv");
-        phoneMap.put('9', "wxyz");
-
-        List<String> res = new ArrayList<>();
-
-        if (digits == null || digits.length() == 0) { //edge case 
-            return res;
-        }
-
-        backtrack(0, new StringBuilder(), digits, phoneMap, res); //i = 0 — starting index , path = "" — current combination, digits = "23", res = [] — result list
-        return res;
+        String[] arr = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        if (digits == null || digits.isEmpty()) return ansList;
+        findCombination(digits, ansList, 0, combi, arr);
+        return ansList;
     }
 
-    private void backtrack(int i, StringBuilder path, String digits, Map<Character, String> phoneMap,List<String> res) {
-        if (i == digits.length()) { //i=0 != 2, then skip 
-            res.add(path.toString());
+    private void findCombination(String digits, List<String> ansList, int index, String combi, String[] arr){
+        if(combi.length() == digits.length()){
+            ansList.add(combi);
             return;
         }
+           String str = arr[digits.charAt(index) - '0'];
 
-        String letters = phoneMap.get(digits.charAt(i)); //Get the digit at current index i, and find the corresponding letters i.e, '2' -> "abc"
-        for (char c : letters.toCharArray()) {
-            path.append(c); //iterate through the letter of digit and add current letter to path i.e, "a"
-            backtrack(i + 1, path, digits, phoneMap, res); //move to the next digit 
-            path.deleteCharAt(path.length() - 1);
+        for(int i=0; i < str.length(); i++){
+            findCombination(digits, ansList, index + 1, combi + str.charAt(i), arr);
         }
     }
 }
