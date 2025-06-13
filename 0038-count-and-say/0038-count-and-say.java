3204a87dc98a4889dbe2b39fad34232134c374ed
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-06-14 00:28:04 +0530

    Time: 2 ms (91.14%), Space: 41.2 MB (84.6%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,26 +1,26 @@
 class Solution {
     public String countAndSay(int n) {
-        String result = "1";
-        for (int i = 1; i < n; i++) {
-            result = describe(result);
+        String result = "1"; // base case
+        for (int i = 1; i < n; i++) { // generate next sequence
+            result = describe(result); 
         }
-        return result;
+        return result; 
     }
 
     private String describe(String s) {
-        StringBuilder sb = new StringBuilder();
-        int count = 1;
+        StringBuilder sb = new StringBuilder(); 
+        int count = 1; // count chars 
 
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == s.charAt(i - 1)) {
-                count++;
+        for (int i = 1; i < s.length(); i++) { 
+            if (s.charAt(i) == s.charAt(i - 1)) {// same as previous
+                count++; // increment count as encountered i.e, if "11" encounter then count = 2;
             } else {
-                sb.append(count).append(s.charAt(i - 1));
-                count = 1;
+                sb.append(count).append(s.charAt(i - 1)); // say and append i.e, for "11" it'll be "21" as 2 is count and 1 is the char encountered 
+                count = 1; // reset count
             }
         }
 
-        sb.append(count).append(s.charAt(s.length() - 1));
-        return sb.toString();
+        sb.append(count).append(s.charAt(s.length() - 1)); // handle last group
+        return sb.toString(); 
     }
 }
