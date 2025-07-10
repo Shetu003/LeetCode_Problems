5a956ffd7a6ae553d5de4d92b22e901dc8792811
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-07-10 12:23:03 +0530

    Time: 1 ms (99.66%), Space: 55.3 MB (75.18%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -2,47 +2,21 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int output[] = new int[nums.length];
 
-        for(int i=0; i<nums.length; i++){
-            output[i] = 1;
+        for(int i = 0; i < nums.length; i++){
+            output[i] = 1; // Fill output array with 1s
         }
 
         int left = 1;
-        for(int i=0; i<nums.length; i++){
-            output[i] *= left;
-            left *= nums[i];
+        for(int i = 0; i < nums.length; i++){
+            output[i] *= left;      // Store current left product
+            left *= nums[i];       // Update left product
         }
 
-        int right=1;
-        for(int i=nums.length-1; i>=0; i--){
-            output[i] *= right;
-            right *= nums[i];
+        int right = 1;
+        for(int i = nums.length - 1; i >= 0; i--){
+            output[i] *= right;     // Multiply with right product
+            right *= nums[i];       // Update right product
         }
         return output;
     }
 }
-
-
-
-// class Solution {
-//     public int[] productExceptSelf(int[] nums) {
-//         int left[] = new int[nums.length];
-//         int right[] = new int[nums.length];
-
-//         left[0] = 1; //starting from index 1 
-//         for(int i=1; i<nums.length; i++){
-//             left[i] = left[i-1] * nums[i-1];
-//         }
-
-//         right[nums.length-1] = 1; //starting from the 2nd last index
-//         for(int i=nums.length-2; i>-1; i--){
-//             right[i] = right[i+1] * nums[i+1];
-//         }
-
-//         int ans[] = new int[nums.length];  //store the answer in new array 
-//         for(int i=0; i<nums.length; i++){
-//             ans[i] = left[i] * right[i];
-//         }
-//         return ans;
-//     }
-// }
-
