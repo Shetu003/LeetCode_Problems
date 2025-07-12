781548e699c18a5352d6f3b63c9427498d43c01d
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-07-12 18:46:57 +0530

    Time: 1 ms (83.23%), Space: 42 MB (78.85%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -2,25 +2,26 @@ class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> result = new ArrayList<>();
 
-        if(numRows == 0) return result;
+        if(numRows == 0) return result; 
 
         List<Integer> firstRow = new ArrayList<>();
-        firstRow.add(1);
+        firstRow.add(1); //add 1 o first row
         result.add(firstRow);
 
-        if(numRows == 1) return result;
+        if(numRows == 1) return result; //if no of rows is 1 then return result(1 as O/P)
 
-        for(int i=1; i<numRows; i++){
-            List<Integer> prevRow = result.get(i-1);
+        for(int i=1; i<numRows; i++){ //for next rows
+            List<Integer> prevRow = result.get(i-1); //prev row
 
             List<Integer> nxtRow = new ArrayList<>();
-            nxtRow.add(1);
 
-            for(int j=0; j<i-1; j++){
+            nxtRow.add(1); //first element of next rows(1)
+
+            for(int j=0; j<i-1; j++){ //add the prev rows elements 
                 nxtRow.add(prevRow.get(j) + prevRow.get(j+1));
             }
 
-            nxtRow.add(1);
+            nxtRow.add(1); //last element of next rows(1)
 
             result.add(nxtRow);
         }
