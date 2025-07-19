952c33198c32074102f484ce917872cff01be14e
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-07-19 22:57:50 +0530

    Time: 0 ms (100%), Space: 41.7 MB (75.49%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -6,16 +6,13 @@ class Solution {
         for(int i=0; i<n; i++){
             fast = fast.next;
         }
-
         if(fast == null){
             return head.next;
         }
-        
         while(fast.next != null){
             slow = slow.next;
             fast = fast.next;
         }
-
         slow.next = slow.next.next;
         return head;
     }
