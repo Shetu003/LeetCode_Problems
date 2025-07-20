c3e412e9490a9b65a5bf15abca1725f490b7c87c
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-07-20 23:58:14 +0530

    Time: 4 ms (83.13%), Space: 68.7 MB (31.59%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -3,15 +3,15 @@ class Solution {
         ListNode slow = head;
         ListNode fast = head;
 
-        while(fast != null && fast.next != null){
+        while (fast != null && fast.next != null) { //find the middle of the LL
             slow = slow.next;
             fast = fast.next.next;
         }
-        slow = reverse(slow);
+        slow = reverse(slow); //from middle break the LL into two part and reverse it
         fast = head;
 
-        while(fast != null && slow != null){
-            if(fast.val != slow.val){
+        while (fast != null && slow != null) { // compare both the half
+            if (fast.val != slow.val) {
                 return false;
             }
             fast = fast.next;
@@ -19,10 +19,11 @@ class Solution {
         }
         return true;
     }
-    public ListNode reverse(ListNode head){
-        ListNode prev=null;
-        ListNode curr=head;
-        while(curr != null){
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
             ListNode next = curr.next;
             curr.next = prev;
             prev = curr;
@@ -31,3 +32,21 @@ class Solution {
         return prev;
     }
 }
+
+//using STACK without reversing the LL
+
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+//         ListNode cur = head;
+//         Stack<Integer> stack = new Stack<>();
+//         while(cur != null){
+//             stack.push(cur.val);
+//             cur = cur.next;
+//         }
+//         cur = head;
+//         while(cur != null && cur.val == stack.pop()){
+//             cur = cur.next;
+//         }
+//         return cur == null;
+//     }
+// }
