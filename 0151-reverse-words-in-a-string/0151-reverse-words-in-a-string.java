4979060af1b92116bfc2c86e715810659e00c784
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-07-21 17:39:43 +0530

    Time: 6 ms (85.49%), Space: 43.4 MB (38.53%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -5,7 +5,7 @@ class Solution {
 
         for(int i=words.length-1; i>=0; i--){
             sb.append(words[i]);
-            sb.append(" ");
+            if(i != 0) sb.append(" ");
         }
         return sb.toString().trim();
     }
