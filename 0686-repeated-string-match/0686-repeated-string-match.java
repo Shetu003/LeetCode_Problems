7bbf9198cac84d10cf8e996b01530a3e1e68ca62
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-07-23 20:56:17 +0530

    Time: 274 ms (12.34%), Space: 45 MB (24.65%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,13 +1,13 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-        int count = 1;
-        String temp = a;
+        int count = 1; //start with 1 copy of a 
+        String temp = a; //temp for repeatition
 
-        while (!a.contains(b)) {
+        while (!a.contains(b)) { // Keep adding a until b is found in a
             a = a + temp;
             count++;
 
-            if (a.length() > b.length() + 2 * temp.length()) {
+            if (a.length() > b.length() + 2 * temp.length()) { // If a gets too long and still doesn't have b, give up
                 return -1;
             }
         }
