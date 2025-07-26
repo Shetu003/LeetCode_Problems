51df850a9c04cf9408718acefcacd9957d96d5dc
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-07-27 00:16:39 +0530

    Time: 0 ms (100%), Space: 41.4 MB (63.93%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,25 +1,27 @@
 class MyStack {
     private Queue<Integer> q;
+
     public MyStack() {
         q = new LinkedList<>();
     }
-    
-    public void push(int x) {
-        q.add(x);
 
-        for(int i=0; i<q.size()-1; i++){
-            q.add(q.poll());
+    public void push(int x) {
+        q.add(x); //Add the new element to the back of the queue
+
+        // Rotate all elements except the newly added one to the back, ensures the new element moves to the front of the queue,
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.add(q.poll()); // Remove the front element and add it to the back
         }
     }
-    
+
     public int pop() {
         return q.poll();
     }
-    
+
     public int top() {
         return q.peek();
     }
-    
+
     public boolean empty() {
         return q.isEmpty();
     }
@@ -33,6 +35,3 @@ class MyStack {
  * int param_3 = obj.top();
  * boolean param_4 = obj.empty();
  */
-
-
- 
