2c32d247602d58a34def9cb1467f6c0812b6c52a
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-08-06 02:00:41 +0530

    Time: 0 ms (100%), Space: 40.6 MB (27.09%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,12 +1,29 @@
+// //Memoization
+// class Solution {
+//     public static int fibonacci(int n,int[] dp){
+//         if(n<=1) return n;
+//         if(dp[n]!=-1) return dp[n];
+//         return dp[n] = fibonacci(n-1,dp)+fibonacci(n-2,dp);
+//     }
+//     public int fib(int n) {
+//         int[] memoization = new int[n+1];
+//         Arrays.fill(memoization,-1);
+//         return fibonacci(n,memoization);
+//     }
+// }
+
+//Tabulation
 class Solution {
-    public static int fibonacci(int n,int[] dp){
-        if(n<=1) return n;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n] = fibonacci(n-1,dp)+fibonacci(n-2,dp);
-    }
     public int fib(int n) {
-        int[] memoization = new int[n+1];
-        Arrays.fill(memoization,-1);
-        return fibonacci(n,memoization);
+        int dp[] = new int[n+1];
+        if(n <= 1){
+            return n;
+        }
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
