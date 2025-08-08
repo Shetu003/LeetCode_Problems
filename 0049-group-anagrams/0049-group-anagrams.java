e30268c4f604320618a9ff604cce4ead9d39939d
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-08-09 01:23:38 +0530

    Time: 7 ms (64.68%), Space: 48 MB (54.93%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,15 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        // Step 1: Create a HashMap where
+        // key = sorted version of string
+        // value = list of words (anagrams) that match the sorted key
         Map<String, List<String>> map = new HashMap<>();
         for(String s: strs){
-            char[] chars = s.toCharArray();
-            Arrays.sort(chars);
-            String key = new String(chars);
+            char[] chars = s.toCharArray(); //break string into array of chars
+            Arrays.sort(chars); //sort the chars
+            String key = new String(chars); //make a string of sorted char
 
             if(!map.containsKey(key)){
-                map.put(key,new ArrayList<>());
+                map.put(key,new ArrayList<>()); //if not in map then add the key 
             }
-            map.get(key).add(s);
+            map.get(key).add(s); //add the original string in arraylist
         }
         return new ArrayList<>(map.values());       
     }
