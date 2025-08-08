1d7eb070a661a4b709276c41b6aae51e365ff72a
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-08-09 02:06:24 +0530

    Time: 0 ms (100%), Space: 45.6 MB (67.04%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,25 +3,25 @@ public class Solution {
         boolean firstRow = false;
         boolean firstCol = false;
 
-        int m = matrix.length;
-        int n = matrix[0].length;
+        int m = matrix.length; //no of rows
+        int n = matrix[0].length; //no of col
 
-        for(int i=0; i<m; i++){
+        for(int i=0; i<m; i++){ //Identify the rows and columns that should be zeroed
             for(int j=0; j<n; j++){
-                if(matrix[i][j] == 0){
-                    if(i == 0){
+                if(matrix[i][j] == 0){ 
+                    if(i == 0){ //if zero found in 1st row
                         firstRow = true;
                     }
-                    if(j == 0){
+                    if(j == 0){ //if zero found in 1st col
                         firstCol = true;
                     }
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+                    matrix[i][0] = 0; // Mark the first element of the row as zero
+                    matrix[0][j] = 0; // Mark the first element of the column as zero
                 }
             }
         }
 
-        for(int i=1; i<m; i++){
+        for(int i=1; i<m; i++){ //if zero found in row or col then set the appropriate cells to zero
             for(int j=1; j<n; j++){
                 if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
@@ -29,13 +29,13 @@ public class Solution {
             }
         }
 
-        if(firstCol){
+        if(firstCol){ //zero out 1st col 
             for(int i=0; i<m; i++){
                 matrix[i][0] = 0;
             }
         }
 
-        if(firstRow){
+        if(firstRow){ //zero out 1st row
             for(int j=0; j<n; j++){
                 matrix[0][j] = 0;
             }
