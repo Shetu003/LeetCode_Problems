63dc03b27508170755870d058527c78ca397c6c9
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-08-28 13:14:54 +0530

    Time: 0 ms (100%), Space: 40.4 MB (63.82%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -1,8 +1,8 @@
 class Solution {
     public int getSum(int a, int b) {
-        while(b != 0){
-            int carry = (a & b) << 1;
-            a = a^b;
+        while(b != 0){ 
+            int carry = (a & b) << 1; //AND finds positions where both bits are 1 , << Shifts carry bits one position left, because carry affects the next higher bit.
+            a = a^b; //XOR adds bits without carrying.
             b = carry;
         }
         return a;
