bed0f2858d6110c7406097323a71632fcc50892a
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-08-30 15:26:28 +0530

    Time: 1 ms (99.54%), Space: 45 MB (73.28%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -4,12 +4,13 @@ class Solution {
 
         while(right < nums.length-1){
             int farthest = 0;
+            // Only iterate within the current window
             for(int i=left; i<=right; i++){
                 farthest = Math.max(farthest, i+nums[i]);
             }
-            left = right + 1;
-            right = farthest;
-            jumps++;
+            left = right + 1; // update left
+            right = farthest; // update right
+            jumps++; //increment jump at each range/window
         }
         return jumps;
     }
