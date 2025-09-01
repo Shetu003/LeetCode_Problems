c53d8624aeb894ff283c2bfa52345e52125ec949
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-09-01 16:41:03 +0530

    Time: 4 ms (97.84%), Space: 45.1 MB (35.28%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,19 +1,18 @@
 class MinStack {
     private Stack<Integer> stack;
     private Stack<Integer> minStack;
-
     public MinStack() {
         stack = new Stack<>();
         minStack = new Stack<>();
     }
-    
+
     public void push(int val) {
         stack.push(val);
         if(minStack.isEmpty()){
             minStack.push(val);
         }
         else{
-            minStack.push(Math.min(val,minStack.peek()));
+            minStack.push(Math.min(val, minStack.peek()));
         }
     }
     
