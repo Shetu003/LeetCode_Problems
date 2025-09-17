1e6f77a60e09d518459dc0099054ecbaf14ddd01
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-09-17 14:58:57 +0530

    Time: 0 ms (100%), Space: 41.1 MB (83.84%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -14,14 +14,15 @@ class Solution {
         dummy.next = head;
         ListNode curr = dummy;
 
-        while(curr.next != null && curr.next.next != null){
-            ListNode swap1 = curr.next; 
+        while (curr.next != null && curr.next.next != null) {
+            ListNode swap1 = curr.next;
             ListNode swap2 = curr.next.next;
-            swap1.next = swap2.next; 
-            swap2.next = swap1;
-            curr.next = swap2;
-            curr = swap1; //update the curr for next pair 
+
+            swap1.next = swap2.next; //Step 1: link first node to the node after the pair
+            swap2.next = swap1; // Step 2: link swap1 after swap2
+            curr.next = swap2; // Step 3: connect curr to the new first node of the pair (swap2)
+            curr = swap1; // Step 4: move curr to the end of the swapped pair (swap1)
         }
-        return dummy.next; //all nodes after dummy 
+        return dummy.next;
     }
 }
