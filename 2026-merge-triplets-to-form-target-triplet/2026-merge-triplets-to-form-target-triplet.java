48b265663ea7a82470e96f8245b76143c5e6db5a
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-10-04 19:57:40 +0530

    Time: 6 ms (43.89%), Space: 121 MB (54.82%) - LeetHub

---
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+        HashSet<Integer> set = new HashSet<>(); 
+        for(int[] t: triplets){
+            //Skipping the triplets that exceeds the target
+            if(t[0] > target[0] || t[1] > target[1] || t[2] > target[2]) continue;
+            for(int i=0;i<t.length;i++){
+                if(t[i]==target[i]) set.add(i);
+            }
+        }
+        //if our set size is 3, means we got our target in triplets array so return true else return false
+        return set.size() == 3;
+    }
+}
