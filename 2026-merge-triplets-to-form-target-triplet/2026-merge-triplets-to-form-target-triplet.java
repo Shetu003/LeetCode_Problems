d88b8d671e0741daed8de7484fc203f8551b0f58
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-10-04 19:57:59 +0530

    Time: 6 ms (43.89%), Space: 121.5 MB (36.38%) - LeetHub

--- 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.java
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.java
@@ -3,7 +3,9 @@ class Solution {
         HashSet<Integer> set = new HashSet<>(); 
         for(int[] t: triplets){
             //Skipping the triplets that exceeds the target
-            if(t[0] > target[0] || t[1] > target[1] || t[2] > target[2]) continue;
+            if(t[0] > target[0] || t[1] > target[1] || t[2] > target[2]){
+                continue;
+            }
             for(int i=0;i<t.length;i++){
                 if(t[i]==target[i]) set.add(i);
             }
