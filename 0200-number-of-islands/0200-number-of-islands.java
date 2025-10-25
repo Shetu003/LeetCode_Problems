f6962a70ae0535aee6f4ad847316d8cefaa6d680
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-10-25 17:36:05 +0530

    Time: 2 ms (99.83%), Space: 49.6 MB (62.28%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -7,23 +7,23 @@ class Solution {
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j] == '1'){
-                    search(grid,i,j);
+                    dfs(grid, i, j);
                     islands++;
                 }
             }
         }
         return islands;
     }
-    private void search(char[][]grid, int i, int j){
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
+    private void dfs(char [][]grid, int i, int j){
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1'){
             return;
         }
-        grid[i][j] = '0'; //mark as visited
+        grid[i][j] = '0';
 
-        search(grid, i-1, j); //move up 
-        search(grid, i+1, j); //move down
-        search(grid, i, j-1); //move left
-        search(grid, i, j+1); //move right
+        dfs(grid, i-1, j);
+        dfs(grid, i+1, j);
+        dfs(grid, i, j-1);
+        dfs(grid, i, j+1);
     }
 }
 
