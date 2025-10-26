90be7b8791799f8f77036b8a1e91de94b3b935f8
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-10-26 15:34:52 +0530

    Time: 6 ms (72.66%), Space: 45.5 MB (54.96%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,24 +1,25 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> graph = new ArrayList<>();
-        int indegree[] = new int[numCourses];
+        int indegree[] = new int[numCourses]; //counts how many prerequisites course has
         for(int i=0; i<numCourses; i++){
             graph.add(new ArrayList<>());
         }
         for(int pair[] : prerequisites){
             int course = pair[0];
             int prereq = pair[1];
-            graph.get(prereq).add(course);
+            graph.get(prereq).add(course); //get the prereq of course and increment indegree
             indegree[course]++;
         }
         Queue<Integer> q = new LinkedList<>();
         for(int i=0; i<numCourses; i++){
-            if(indegree[i] == 0){
+            if(indegree[i] == 0){ //courses witb indegree 0 taken 1st
                 q.add(i);
             }
         }
         int count = 0;
 
+        //bfs traversal
         while(!q.isEmpty()){
             int curr = q.poll();
             count++;
@@ -29,6 +30,6 @@ class Solution {
                 }
             }
         }
-        return count == numCourses;
+        return count == numCourses; //Check if All Courses Finished
     }
 }
